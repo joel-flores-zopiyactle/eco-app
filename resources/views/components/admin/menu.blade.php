545933a471f1ad695711102aff66b14ea4be0eff
Joel <joefloreszopiyactle@gmail.com>
2023-11-17 20:35:53 -0600

    Updated icons

--- resources/views/components/admin/menu.blade.php
+++ resources/views/components/admin/menu.blade.php
@@ -1,14 +1,10 @@
-<nav class="nav flex-column menu-admin">
-    <a class="nav-link d-flex align-items-center" aria-current="page" href="{{route('documents', 'all')}}">
-        <span class="material-symbols-outlined">
-            description
-        </span>
+<nav class="nav flex-column menu-admin mt-2">
+    <a class="d-flex align-items-center btn btn-outline-dark rounded-pill mb-3" aria-current="page" href="{{route('documents', 'all')}}">
+        <img class="me-2" src="{{asset('assets/svg/document.svg')}}" alt="document" width="24">
         Documentos
     </a>
-    <a class="nav-link d-flex align-items-center" aria-current="page" href="{{route('categories')}}">
-        <span class="material-symbols-outlined">
-            category
-        </span>
+    <a class="d-flex align-items-center btn btn-outline-dark rounded-pill" aria-current="page" href="{{route('categories')}}">
+        <img class="me-2" src="{{asset('assets/svg/category.svg')}}" alt="document" width="24">
         Categorías
     </a>
 </nav>
